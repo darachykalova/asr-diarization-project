45e047a67e89bdf91f852d60ab4ea32994ee232e
Daria Chikalova <darachykalova@gmail.com>
2026-06-09 11:30:51 +0300

    Add Docker environment and configuration

--- celery_app.py
+++ celery_app.py
@@ -1,10 +1,22 @@
+import os
+
 from celery import Celery
 
 
+REDIS_URL = os.getenv(
+    "REDIS_URL",
+    "redis://localhost:6379/0"
+)
+
+REDIS_BACKEND_URL = os.getenv(
+    "REDIS_BACKEND_URL",
+    "redis://localhost:6379/1"
+)
+
 celery_app = Celery(
     "asr_diarization_worker",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/1"
+    broker=REDIS_URL,
+    backend=REDIS_BACKEND_URL
 )
 
 celery_app.conf.update(
